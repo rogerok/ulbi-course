d99759a17a8f7cc5fe433f3fbc5475144aa60ed1
vyacheslav <fedorik83@gmail.com>
2023-04-23 17:04:46 +0300

    added translation

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,17 +1,18 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useState } from 'react';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
-import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
-import { Button } from 'shared/ui/Button/Button';
-import cls from './Sidebar.module.scss';
+import { classNames } from "shared/lib/classNames/classNames";
+import { useState } from "react";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
+import { Button } from "shared/ui/Button/Button";
+import { useTranslation } from "react-i18next";
+import cls from "./Sidebar.module.scss";
 
 interface SidebarProps {
-    className?: string;
+  className?: string;
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
-
+    const { t } = useTranslation();
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
@@ -25,7 +26,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 data-testid="sidebar-toggle"
                 onClick={onToggle}
             >
-                toggle
+                {t("Переключатель")}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
