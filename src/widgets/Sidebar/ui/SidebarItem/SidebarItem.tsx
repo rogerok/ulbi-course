fa42f20b3429f9c9741a7d8b105f7642eef5cbb4
vyacheslav <fedorik83@gmail.com>
2023-07-23 16:25:28 +0300

    added jest testing

--- src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -5,27 +5,25 @@ import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 import { SidebarItemType } from "../../model/item";
 import cls from "./SidebarItem.module.scss";
 
-
 interface SidebarItemProps {
   item: SidebarItemType;
   collapsed: boolean;
 }
 
-export const SidebarItem: FC<SidebarItemProps> = memo((props: SidebarItemProps) => {
+export const SidebarItem: FC<SidebarItemProps> = memo(
+  (props: SidebarItemProps) => {
     const { t } = useTranslation();
     const { item, collapsed } = props;
 
-
     return (
-
-        <AppLink theme={AppLinkTheme.SECONDARY}
-            to={item.path}
-            className={classNames(cls.item, { [cls.collapsed]: collapsed })}
-        >
-            <item.Icon className={cls.icon} />
-            <span className={cls.link}>
-                {t(item.text)}
-            </span>
-        </AppLink>
+      <AppLink
+        theme={AppLinkTheme.SECONDARY}
+        to={item.path}
+        className={classNames(cls.item, { [cls.collapsed]: collapsed })}
+      >
+        <item.Icon className={cls.icon} />
+        <span className={cls.link}>{t(item.text)}</span>
+      </AppLink>
     );
-});
+  }
+);
