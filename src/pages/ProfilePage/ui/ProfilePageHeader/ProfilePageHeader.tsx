557de2ce0c7f97d746c23c7b58a7eabd125d7027
v.fedorik <fedorik833@gmail.com>
2023-12-08 16:19:29 +0300

    created Listbox component

--- src/pages/ProfilePage/ui/ProfilePageHeader/ProfilePageHeader.tsx
+++ src/pages/ProfilePage/ui/ProfilePageHeader/ProfilePageHeader.tsx
@@ -2,9 +2,12 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Text } from 'shared/ui/Text/Text';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import {
-  getProfileData, getProfileReadonly, profileActions, updateProfileData,
+  getProfileData,
+  getProfileReadonly,
+  profileActions,
+  updateProfileData,
 } from 'entities/Profile';
 import { useCallback } from 'react';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
@@ -12,13 +15,11 @@ import { getUserAuthData } from 'entities/User';
 import cls from './ProfilePageHeader.module.scss';
 
 interface ProfilePageHeaderProps {
-    className?: string;
+  className?: string;
 }
 
 export const ProfilePageHeader = (props: ProfilePageHeaderProps) => {
-  const {
-    className,
-  } = props;
+  const { className } = props;
 
   const { t } = useTranslation('profile');
   const authData = useSelector(getUserAuthData);
@@ -40,38 +41,36 @@ export const ProfilePageHeader = (props: ProfilePageHeaderProps) => {
   }, [dispatch]);
 
   return (
-      <div className={classNames(cls.ProfilePageHeader, {}, [className])}>
-        <Text title={t('Профиль')} />
-        {canEdit && (
-      <div className={cls.btnsWrapper}>
-            {readonly
-            ? (
-                <Button
-                  className={cls.editBtn}
-                  theme={ButtonTheme.OUTLINE}
-                  onClick={onEdit}
+    <div className={classNames(cls.ProfilePageHeader, {}, [className])}>
+      <Text title={t('Профиль')} />
+      {canEdit && (
+        <div className={cls.btnsWrapper}>
+          {readonly ? (
+            <Button
+              className={cls.editBtn}
+              theme={ButtonTheme.OUTLINE}
+              onClick={onEdit}
+            >
+              {t('Редактировать')}
+            </Button>
+          ) : (
+            <>
+              <Button
+                className={cls.editBtn}
+                theme={ButtonTheme.OUTLINE_RED}
+                onClick={onCancelEdit}
               >
-                  {t('Редактировать')}
+                {t('Отменить')}
               </Button>
-            )
-            : (
-                <>
-                  <Button
-                    className={cls.editBtn}
-                    theme={ButtonTheme.OUTLINE_RED}
-                    onClick={onCancelEdit}
-                >
-                    {t('Отменить')}
-                </Button>
-                  <Button
-                    className={cls.saveBtn}
-                    theme={ButtonTheme.OUTLINE}
-                    onClick={onSave}
-                >
-                    {t('Сохранить')}
-                </Button>
-              </>
-            )}
+              <Button
+                className={cls.saveBtn}
+                theme={ButtonTheme.OUTLINE}
+                onClick={onSave}
+              >
+                {t('Сохранить')}
+              </Button>
+            </>
+          )}
         </div>
       )}
     </div>
---
+++ src/shared/ui/ListBox/ListBox.tsx
@@ -0,0 +1,70 @@
+import { Listbox } from '@headlessui/react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { ReactNode } from 'react';
+import cls from './ListBox.module.scss';
+
+export interface ListBoxOptions<T> {
+  value?: T;
+  content: ReactNode;
+}
+
+interface ListBoxProps<T extends string> {
+  className?: string;
+  label?: string;
+  value?: T;
+  onChange?: (value: T) => void;
+  readonly?: boolean;
+  options?: ListBoxOptions<T>[];
+  defaultValue?: T;
+}
+
+export const ListBox = <T extends string>(props: ListBoxProps<T>) => {
+  const { className, defaultValue, label, options, onChange, value, readonly } =
+    props;
+
+  return (
+    <Listbox
+      as="div"
+      className={classNames(cls.ListBox, {}, [className])}
+      value={value}
+      defaultValue={defaultValue}
+      onChange={onChange}
+      disabled={readonly}
+    >
+      {({ open }) => (
+        <>
+          <Listbox.Button className={cls.trigger}>
+            <Button className={cls.button} theme={ButtonTheme.CLEAR}>
+              <span>{value ?? defaultValue}</span>
+              <span className={cls.arrows}>{open ? '↑' : '↓'}</span>
+            </Button>
+          </Listbox.Button>
+          {open && (
+            <Listbox.Options className={cls.options}>
+              {!!options?.length &&
+                options.map((option) => (
+                  <Listbox.Option
+                    className={cls.option}
+                    key={option.value}
+                    value={option.value}
+                  >
+                    {({ active, selected }) => (
+                      <li
+                        className={classNames(cls.optionInner, {
+                          [cls.active]: active,
+                          [cls.selected]: selected,
+                        })}
+                      >
+                        {option.content}
+                      </li>
+                    )}
+                  </Listbox.Option>
+                ))}
+            </Listbox.Options>
+          )}
+        </>
+      )}
+    </Listbox>
+  );
+};
